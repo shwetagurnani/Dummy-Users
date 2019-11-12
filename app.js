727271cdba5cb64a6c3c56e1b49b46a79f2f6005
shwetagurnani <shwetaagra28@gmail.com>
2019-11-13 01:07:17 +0530

    app.js file updated and linked with the algorithm

--- app.js
+++ app.js
@@ -16,14 +16,124 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use(formRoutes);
 app.get('/',(req, res) =>{
-    // data.fetchAll()
-    // .then(data=>{
+    
+    const db=require('./util/database');
+
+var x=new Date();
+var curDay=x.getDate();//current date
+var curMonth=x.getMonth()+1;//current month
+var curYear=x.getFullYear();//current year
+var curHr=x.getHours();
+var curMm=x.getMinutes();
+
+function calcY()
+{
+    var set=Math.floor((Math.random()*(11-1)+1));
+    if(set>=1&&set<=4)
+    return ( Math.random()*(1.40-1.00)+1.00);
+    else if(set>=5&&set<=7)
+    return ( Math.random()*(1.70-1.40)+1.40);
+    else if(set>=8&&set<=9)
+    return ( Math.random()*(1.90-1.70)+1.70);
+    else
+    return ( Math.random()*(2.00-1.90)+1.90);
+}
+
+var d,m,y,h,mn,valueY,valueX,ny,newy;
+var sum=0,k=0;
+db.query('SELECT * from data',(error,results)=>
+{
+   // console.log(results);
+  //console.log(results[k]);
+if(error)
+console.log(error);
+
+else
+{
+      d=results[k].day;//day the dummy views last updated
+      m=results[k].month;//month the dummy views last updated
+      y=results[k].year;//year the dummy views last updated
+      h=results[k].hour;//hour
+      mn=results[k].minute;//minute
+      valueY=results[k].y;//value of y
+      valueX=results[k].x;//value of x
+     
+ if(d!=curDay && curDay>d)
+{
+const monthDays = [31, 28, 31, 30, 31, 30,31, 31, 30, 31, 30, 31];
+
+function countLeapYears(d,m,y)
+{
+  if(m<=2)
+  y=y-1;
+  return (Math.floor(y/4)-Math.floor(y/100)+Math.floor(y/400));
+}
+var n1 = y*365 + d; 
+  
+    for (var i=0; i<m - 1; i++) 
+        n1 =n1+ monthDays[i]; 
+      
+     n1 =n1+ countLeapYears(d,m,y); 
+  
+    var n2 = curYear*365 + curDay; 
+    for (var i=0; i<curMonth - 1; i++) 
+        n2 =n2+ monthDays[i]; 
+    n2 =n2+ countLeapYears(curDay,curMonth,curYear); 
+    ans=(n2 - n1-1); 
+    if(ans<0)
+    ans=0;
+    
+    //to find the number of minutes on te day of upload
+    var MinBefore=1440-h*60-mn;
+    sum=sum+Math.ceil((MinBefore*valueX*valueY)/(24*60));
+   
+   
+    //to find the number of minutes on that particular day
+    
+    var MinAfter=curHr*60+curMm;
+    ny=calcY();
+   
+    newy = ny.toFixed(2);
+    db.query('UPDATE data SET  y=? where id=?',[newy,k]);        
+    
+    sum=sum+Math.ceil((MinAfter*valueX*newy)/(24*60));
+   
+    for(var i=1;i<=ans;i++)
+    {
+    var ny= calcY();
+    var y=ny.toFixed(2);
+    sum=sum+Math.ceil((i*valueX*y));
+   
+    }  
+}
+else
+{ 
+    var diff=curHr*60+curMm-h*60-mn;
+    sum=sum+diff*valueX*valueY;
+    newy=valueY;
+}
+   
+}
+});
+
+//y=newy,day=curDay, month=curMonth,year=curYear,hour=curHr,minute=curMm
+db.query('UPDATE data SET day=?,month=?,year=?,hour=?,minute=? where id=?',[curDay,curMonth,curYear,curHr,curMm,k],(error,results)=>
+{
+if(error)
+{
+    console.log(error);
+}
+else
+console.log("connected");
+});
+
+
+
     res.render('form');
 });
 
 app.get('/p',function(req,res){
   db.query("UPDATE view SET VIEWS=VIEWS+1 WHERE id=1");
-  //console.log();
 });
 
 app.listen(3000);
