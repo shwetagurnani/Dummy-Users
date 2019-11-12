26986a31a00532691c6460d5afd8dda4c080e52f
shwetagurnani <shwetaagra28@gmail.com>
2019-11-12 12:27:22 +0530

    DummyUsers.js updated

--- DummyUsers.js
+++ DummyUsers.js
@@ -8,8 +8,8 @@ var curHr=x.getHours();
 var curMm=x.getMinutes();
 
 
-
-var d,m,y,h,m,valueY,valueX,newy;
+var sum=0;
+var d,m,y,h,m,valueY,valueX,ny;
 db.query('SELECT * from data where id=1',(error,results)=>
 {
 if(error)
@@ -20,20 +20,13 @@ else
       m=results[0].month;//month the dummy views last updated
       y=results[0].year;//year the dummy views last updated
       h=results[0].hour;//hour
-      m=results[0].month;//minute
+      mn=results[0].minute;//minute
       valueY=results[0].y;//value of y
       valueX=results[0].x;
-      console.log(d);
-      
-}
-});
-
-var sum=0;
-
-console.log(d);
-if(d!=curDay && curDay>d)
+     
+     
+ if(d!=curDay && curDay>d)
 {
-    console.log('shweta');
 const monthDays = [31, 28, 31, 30, 31, 30,31, 31, 30, 31, 30, 31];
 
 function countLeapYears(d,m,y)
@@ -49,39 +42,55 @@ var n1 = y*365 + d;
   
      n1 =n1+ countLeapYears(d,m,y); 
   
-    var n2 = y1*365 + d1; 
-    for (var i=0; i<m1 - 1; i++) 
+    var n2 = curYear*365 + curDay; 
+    for (var i=0; i<curMonth - 1; i++) 
         n2 =n2+ monthDays[i]; 
     n2 =n2+ countLeapYears(curDay,curMonth,curYear); 
     ans=(n2 - n1-1); 
     if(ans<0)
     ans=0;
-
    
     //to find the number of minutes on te day of upload
-    var MinBefore=1440-h*60-m;
-    console.log(MinBefore);
-    sum=sum+MinBefore*valX*valY;
+    var MinBefore=1440-h*60-mn;
+    sum=sum+((MinBefore*valueX*valueY)/(24*60));
+    sum=Math.ceil(sum);
 
     //to find the number of minutes on that particular day
-   
+    // console.log("sum is"+sum);
     var MinAfter=curHr*60+curMm;
-     newy=Math.floor(Math.random() * 100)+1;
-    sum=sum+MinAfter*x*newy;
+    ny=(Math.random())+1;
+    var newy = ny.toFixed(2);      
+    console.log(newy);
+    console.log(MinAfter);
+    console.log(valueX);
    
-    for(var i=2;i<ans;i++)
+    sum=sum+((MinAfter*valueX*newy)/(24*60));
+    sum=Math.ceil(sum);
+    // console.log("sum is"+sum);
+    for(var i=1;i<=ans;i++)
     {
-    var y=Math.floor(Math.random() * 100)+1;
-    sum=sum+i*x*y;
+    var ny= (Math.random())+1;
+    var y=ny.toFixed(2);
+    sum=sum+(i*valueX*y);
+    sum=Math.ceil(sum);
+    console.log("x is"+valueX);
+    console.log("y is"+y);
+    console.log("sum is"+sum);
     }
+   
 }
 else
 { 
     var diff=curHr*60+curMm-h*60-m;
     sum=sum+diff*x*y;
 }
+      
+}
+
+
+
+});
+
+
 
 //db.query('UPDATE data SET y=newy,day=curDay,month=curMonth,year=curYear,hour=curHr,minute=curMm where id=1');
-
-
-console.log(sum);
